58c2a8f7edf991db181c92541c33841ef45c5fc8
stef.lauwaet <stef.lauwaet@student.howest.be>
2020-06-12 17:46:13 +0200

    draw canvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", init);
 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 function init(){
-    console.log("loaded");
+    drawBoard();
+}
+
+function drawBoard(){
+
+    ctx.strokeStyle = "#C5DCA0"
+    ctx.strokeRect(0, 0, 800, 500);
 }
